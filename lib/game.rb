4441da3405244e8408f83dfba1255874a2fdb4d1
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-10-20 12:41:41 +0100

    added a 3x3 2-dimensional array to array as grid

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,11 @@
 class Game
+  attr_reader :grid
 
+  def initialize
+    @grid = [[nil,nil,nil],
+             [nil,nil,nil],
+             [nil,nil,nil]]
+            
+  end
+  
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,17 @@
 require 'game'
 
 describe Game do
-  
+
+  describe '#initialize' do
+    it 'creates a grid which is a 3 x 3 dimensional array' do
+      game = Game.new
+      expect(game.grid.length).to eq 3  
+    end
+
+    it 'creates a grid which is a 3 x 3 dimensional array' do
+      game = Game.new
+      expect(game.grid[0].length).to eq 3  
+    end
+  end
+
 end
