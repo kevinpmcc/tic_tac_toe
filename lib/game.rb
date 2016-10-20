217c59a784a1278bfad116bfa4beb8ace05eb079
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-10-20 13:00:02 +0100

    added features where turns change, players can choose fields alternately, will display correct symbol (x or y) based on whose turn

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,28 @@
 class Game
-  attr_reader :grid
+  attr_reader :grid, :player_1_turn
 
   def initialize
     @grid = [[nil,nil,nil],
              [nil,nil,nil],
              [nil,nil,nil]]
-            
+    @player_1_turn = true
+  end
+
+  def choose_field(index1, index2)
+    @grid[index1][index2] = current_player_symbol
+    change_turn
   end
   
+  def player_1_turn
+    @player_1_turn
+  end
+
+  def change_turn
+    @player_1_turn = !@player_1_turn
+  end
+
+  def current_player_symbol
+    player_1_turn ? 'X' : 'Y'
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -14,4 +14,39 @@ describe Game do
     end
   end
 
+  describe '#choose_field' do
+    it 'at start of game it takes an argument and changes the field in the array to an x' do
+      game = Game.new
+      game.choose_field(0,0)
+      expect(game.grid).to eq [['X',nil,nil],[nil,nil,nil],[nil,nil,nil]]
+    end
+
+    it 'after one turn it changes the field in the array to a y' do
+      game = Game.new
+      game.choose_field(0,0)
+      game.choose_field(0,1)
+      expect(game.grid).to eq [['X','Y',nil],[nil,nil,nil],[nil,nil,nil]]
+    end
+  end
+
+  describe '#player_1_turn' do
+    it 'starts with player1s turn being true' do
+      game = Game.new
+      expect(game.player_1_turn).to eq true
+    end
+
+    it 'after choose field is called,  turn player_1_turn becomes false' do
+      game = Game.new
+      game.choose_field(0,0)
+      expect(game.player_1_turn).to eq false
+    end
+
+    it 'after choose_field is called_twice, turn player_1 turn becomes true again' do
+      game = Game.new
+      game.choose_field(0,0)
+      game.choose_field(0,0)
+      expect(game.player_1_turn).to eq true
+    end
+
+  end
 end
